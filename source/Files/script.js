6ff7aa63d60f8812edee5075e3b184a23170782c
Daniell Mesquita <daniellmesquita@protonmail.com>
2019-09-25 20:30:49 -0300

    Shaming but amazing creating a iframe-based browser
    Signed-off-by: Daniell Mesquita <daniellmesquita@protonmail.com>

--- source/Files/script.js
+++ source/Files/script.js
@@ -1,5 +1,33 @@
+var ldngscrn = document.getElementById("loadingscreen");
+
+    const brwsrfrm = document.getElementById("browserframe");
+
+    brwsrfrm.contentWindow.onbeforeunload = function () {
+        ldngscrn.style.display = "initial";
+    };
+
+    brwsrfrm.onbeforeunload = function () {
+        alert("Please input a Value");
+    };
+
+/* Spoof user-agent in browserframe > */
+function setUserAgent(window, userAgent) {
+    if (window.navigator.userAgent != userAgent) {
+        var userAgentProp = { get: function () { return userAgent; } };
+        try {
+            Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+        } catch (e) {
+            window.navigator = Object.create(navigator, {
+                userAgent: userAgentProp
+            });
+        }
+    }
+}
+/* < */
+
 /* Only displays enter button if input haves valid text > */
 var flght = document.getElementById("flght");
+var refreshbtn = document.getElementById("refresh");
 
 const input = document.querySelector('input')
 
@@ -8,54 +36,101 @@ input.addEventListener('input', evt => {
   
   if (!value) {
     input.dataset.state = ''
+    refreshbtn.style.display = "inline";
     flght.style.display = "none";
   }
   
   const trimmed = value.trim()
   
   if (trimmed) {
-    flght.style.display = "block";
+    flght.style.display = "inline";
+    refreshbtn.style.display = "none";
     input.dataset.state = 'valid'
   } else {
+    refreshbtn.style.display = "inline";
     flght.style.display = "none";
     input.dataset.state = 'invalid'
   }
 })
 /* < */
 
-/* get text from input > */
-        function getInputValue(){
+/**
+ * Creates a read/writable property which returns a function set for write/set (assignment)
+ * and read/get access on a variable
+ *
+ * @param {Any} value initial value of the property
+ */
+function createProperty(value) {
+    var _value = value;
 
-            // Selecting the input element and get its value 
+    /**
+     * Overwrite getter.
+     *
+     * @returns {Any} The Value.
+     * @private
+     */
+    function _get() {
+        return _value;
+    }
 
-            var inputVal = document.getElementById("input").value;
+    /**
+     * Overwrite setter.
+     *
+     * @param {Any} v   Sets the value.
+     * @private
+     */
+    function _set(v) {
+        _value = v;
+    }
 
-            
+    return {
+        "get": _get,
+        "set": _set
+    };
+};
 
-            // Displaying the value
+/**
+ * Creates or replaces a read-write-property in a given scope object, especially for non-writable properties.
+ * This also works for built-in host objects (non-DOM objects), e.g. navigator. 
+ * Optional an initial value can be passed, otherwise the current value of the object-property will be set.
+ *
+ * @param {Object} objBase  e.g. window
+ * @param {String} objScopeName    e.g. "navigator"
+ * @param {String} propName    e.g. "userAgent"
+ * @param {Any} initValue (optional)   e.g. window.navigator.userAgent
+ */
+function makePropertyWritable(objBase, objScopeName, propName, initValue) {
+    var newProp,
+        initObj;
 
-            alert(inputVal);
-
+    if (objBase && objScopeName in objBase && propName in objBase[objScopeName]) {
+        if(typeof initValue === "undefined") {
+            initValue = objBase[objScopeName][propName];
         }
-/* < */
 
-/* If input haves valid text and it is submitted, update iframe URL with input's text > */
-function required()
-{
-var empt = document.forms["urlsubmit"]["urltext"].value;
-if (empt == "")
-{
-alert("Please input a Value");
-}
-else 
-{
-document.getElementById('browserframe').src = inputVal;
-}
-}
-/* < */
+        newProp = createProperty(initValue);
 
-/* knows if iframe has been loaded > */
-function frameload(){
- alert("iframe loaded")
-}
-/* < */
+        try {
+            Object.defineProperty(objBase[objScopeName], propName, newProp);
+        } catch (e) {
+            initObj = {};
+            initObj[propName] = newProp;
+            try {
+                objBase[objScopeName] = Object.create(objBase[objScopeName], initObj);
+            } catch(e) {
+                // Workaround, but necessary to overwrite native host objects
+            }
+        }
+    }
+};
+
+makePropertyWritable(window, "navigator", "userAgent");
+
+window.navigator.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586";
+
+console.log(window.navigator.userAgent);
+
+var __originalNavigator = navigator;
+navigator = new Object();
+navigator.__proto__ = __originalNavigator;
+navigator.__defineGetter__('userAgent', function () { return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'; });
